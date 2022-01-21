347b25bf8218d8e4de3a8d9d3c995ebc97acb768
sparklyballs <sparklyballs@gmail.com>
2022-01-21 21:11:49 +0000

    use latest chia style with separate build and service env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,8 @@ cd /stor-blockchain || exit 1
 stor init --fix-ssl-permissions
 
 if [[ ${testnet} == 'true' ]]; then
-   echo "configure testnet"
-   stor configure --testnet true
+  echo "configure testnet"
+  stor configure --testnet true
 fi
 
 if [[ ${keys} == "persistent" ]]; then
@@ -26,7 +26,7 @@ elif [[ ${keys} == "generate" ]]; then
 elif [[ ${keys} == "copy" ]]; then
   if [[ -z ${ca} ]]; then
     echo "A path to a copy of the farmer peer's ssl/ca required."
-	exit
+    exit
   else
   stor init -c "${ca}"
   fi
@@ -35,12 +35,14 @@ else
 fi
 
 for p in ${plots_dir//:/ }; do
-    mkdir -p "${p}"
-    if [[ ! $(ls -A "$p") ]]; then
-        echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
-    fi
-    stor plots add -d "${p}"
+  mkdir -p "${p}"
+  if [[ ! $(ls -A "$p") ]]; then
+    echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+  fi
+  stor plots add -d "${p}"
 done
+
+stor configure --upnp "${upnp}"
 
 if [[ -n "${log_level}" ]]; then
   stor configure --log-level "${log_level}"
@@ -54,6 +56,30 @@ if [[ -n "${outbound_peer_count}" ]]; then
   stor configure --set_outbound-peer-count "${outbound_peer_count}"
 fi
 
-sed -i 's/localhost/127.0.0.1/g' "$CONFIG_ROOT/config/config.yaml"
+if [[ -n ${farmer_address} && -n ${farmer_port} ]]; then
+  stor configure --set-farmer-peer "${farmer_address}:${farmer_port}"
+fi
+
+sed -i 's/localhost/127.0.0.1/g' "$STOR_ROOT/config/config.yaml"
+
+if [[ ${log_to_file} != 'true' ]]; then
+  sed -i 's/log_stdout: false/log_stdout: true/g' "$STOR_ROOT/config/config.yaml"
+else
+  sed -i 's/log_stdout: true/log_stdout: false/g' "$STOR_ROOT/config/config.yaml"
+fi
+
+# Map deprecated legacy startup options.
+if [[ ${farmer} == "true" ]]; then
+  service="farmer-only"
+elif [[ ${harvester} == "true" ]]; then
+  service="harvester"
+fi
+
+if [[ ${service} == "harvester" ]]; then
+  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
+    echo "A farmer peer address, port, and ca path are required."
+    exit
+  fi
+fi
 
 exec "$@"
